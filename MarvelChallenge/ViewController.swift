1815e5bdc19710e82dd0a0c909791b35c1756404
Bienvenido Fernández Lora <bienvenido.fernandez@iecisa.com>
2022-11-07 21:35:28 +0100

    Initial Commit

---
+++ MarvelChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelChallenge
+//
+//  Created by 67881458 on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarvelChallengeUITests/MarvelChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MarvelChallengeUITestsLaunchTests.swift
+//  MarvelChallengeUITests
+//
+//  Created by 67881458 on 7/11/22.
+//
+
+import XCTest
+
+final class MarvelChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
